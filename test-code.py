976ae988d9c7b4821bea869c813ee7f75c6eeae7
root <root@noob12-coding.fritz.box>
2022-01-31 10:03:28 +0100

    the python code has been updated

--- test-code.py
+++ test-code.py
@@ -1,4 +1,4 @@
-	# Python3 example on Raspberry Pi to handle notification from
+# Python3 example on Raspberry Pi to handle notification from
 # ESP32 BLE_notify example.
 # To install bluepy for Python3:
 # $ sudo pip3 install bluepy
@@ -7,17 +7,58 @@ from bluepy import btle
 import matplotlib.pyplot as plt
 import time
 from os import system
-
+import tkinter as tk
 from tkinter import *
-
+#from PIL import ImageTk,Image
 window = Tk()
-window.geometry('600x600')
 
-window.title("Weights of the minibar")
+window.title("Minibar")
 
-window.mainloop()
+window.geometry('400x400')
 
+lbl = Label(window, text="Click  to connect to the ESP32 board via BLE       ")
+say = Label(window)
+lbl.grid(column=50, row=20)
+say.grid(column=100, row=100)
 
+condition=True 
+ 
+
+def infinite_loop(): 
+   if condition: 
+      # lbl.configure(text=str(data[0]))
+
+        
+
+       p.waitForNotifications(1.0)
+       
+   # Call the infinite_loop() again after 1 sec 
+  
+       window.after(500, infinite_loop) 
+   
+      
+def start():
+          condition=True 
+          say.configure(text="  connecting to Esp32 board BLE " )
+          time.sleep(7) 
+      
+def stop():
+       
+          condition=False 
+          window.destroy()
+
+#def clicked():
+lbl.configure(text="Connecting to ESP32 BLE")
+time.sleep(4)
+   #    window.destroy()
+
+btn1 = Button(window, text="start ", command=start)
+btn1.place(x=100, y=50)
+#btn.grid(column=3, row=3)
+
+btn = Button(window, text="Disconnect ESP32 ", command=stop)
+btn.place(x=100, y=150)
+#btn.grid(column=100, row=100)
 
 """value = [0]*30
 
@@ -37,7 +78,12 @@ class MyDelegate(btle.DefaultDelegate):
         #print(type(data))
         #print(dir(data))
        # print(data)
-        print("The weight readings are :  "+   str(int(data[0])) + " grams\n" )
+
+        lbl.configure(text="  The reading is  :   " + str(data[0]) + "  Grams",font=("Helvetica",18),bd=3,relief="sunken")
+  
+        label=Label(window,text="hi",font=("Helvetica",18),bd=1,relief="sunken",justify="right")
+     #   label.pack(pady=20,ipady=10,ipadx=10)
+        print("The weight readings are :  "+   str(data[0]) + " grams\n" )
         time.sleep(0.6)
         
   #      value.pop(0)
@@ -73,18 +119,29 @@ ch_data = p.readCharacteristic(ch.valHandle + 1)
 #ch_data_02 = p.readCharacteristic(ch_02.valHandle + 1)
 
 """print(type(ch_data))"""
-print(ch_data)
-print("hellllloooooooooooooooooooooooo")
+
+#print(ch-data
+
+print("connecting to ESP32")
 print("        ")
 #print(ch_data_02)
 
-print("=== Main Loop ===")
+print("Connected.... Receiving Data Via BLE")
+print("                                     ")
+#while True:
+     
 
-while True:
-    if p.waitForNotifications(1.0):
+
+         #if p.waitForNotifications(1.0):
+#window.mainloop()
+                #print("hi")
+#    if p.waitForNotifications(1.0):
                      
 # handleNotification() was called
-          continue
+               # continue
       
     #    print("Waiting...")
     # Perhaps do something else here
+window.after(500, infinite_loop) 
+         
+window.mainloop()
